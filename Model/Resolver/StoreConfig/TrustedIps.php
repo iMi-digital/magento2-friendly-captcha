f55f86e82d508cc354d5521cabdf88b2775a678b
Dimitri BOUTEILLE <dimitri.bouteille@reflet-digital.com>
2025-12-18 11:52:07 +0100

    💎 Add graphQL support

---
+++ Model/Resolver/StoreConfig/TrustedIps.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ *  Copyright © iMi digital GmbH, based on work by MageSpecialist
+ *  See LICENSE for license details.
+ */
+
+namespace IMI\FriendlyCaptcha\Model\Resolver\StoreConfig;
+
+use IMI\FriendlyCaptcha\Model\Config;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+class TrustedIps implements ResolverInterface
+{
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * TrustedIps Constructor
+     *
+     * @param Config $config
+     */
+    public function __construct(
+        Config $config
+    ) {
+        $this->config = $config;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        return $this->config->getTrustedIps();
+    }
+}
