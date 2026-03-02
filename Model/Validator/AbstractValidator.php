b4eaa5b2cd1b867514a6bbab31dbb0e49a0ced9f
Dimitri BOUTEILLE <dimitri.bouteille@reflet-digital.com>
2026-03-02 11:17:23 +0100

    V2 Support: CS and add doc in AbstractValidator

--- Model/Validator/AbstractValidator.php
+++ Model/Validator/AbstractValidator.php
@@ -4,7 +4,17 @@ namespace IMI\FriendlyCaptcha\Model\Validator;
 
 use IMI\FriendlyCaptcha\Model\Config;
 use Magento\Framework\HTTP\Client\Curl;
+use Magento\Framework\HTTP\Client\CurlFactory;
+use Magento\Framework\Serialize\Serializer\Json;
 
+/**
+ * Base class for Friendly Captcha solution validators.
+ *
+ * Provides shared dependencies and helper methods used by concrete validator
+ * implementations (e.g. ValidatorV1, ValidatorV2). Each concrete validator is
+ * responsible for building the HTTP request according to its API version and
+ * calling {@see isSuccessResponse()} to evaluate the result.
+ */
 abstract class AbstractValidator
 {
     /**
@@ -23,8 +33,6 @@ abstract class AbstractValidator
     protected $serializer;
 
     /**
-     * AbstractValidator constructor.
-     *
      * @param Config $config
      * @param CurlFactory $curlFactory
      * @param Json $serializer
@@ -40,13 +48,19 @@ abstract class AbstractValidator
     }
 
     /**
+     * Determine whether the Friendly Captcha verification API returned a successful response.
+     *
+     * A response is considered successful when the HTTP status code is 200 and
+     * the decoded JSON body contains a truthy `success` field.
+     *
      * @param Curl $curl
      * @param array $response
-     * @return bool
+     * @return bool True if the captcha solution was accepted, false otherwise
      */
     protected function isSuccessResponse(Curl $curl, array $response): bool
     {
         $success = $response['success'] ?? false;
+
         return $curl->getStatus() === 200 && $success === true;
     }
 }
