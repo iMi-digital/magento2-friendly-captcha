c4af75c0151e8c0dc986be8153a1feae230e26ec
Alexander Menk <a.menk@imi.de>
2026-04-22 12:27:44 +0200

    #65 GitHub Action: Add basic integration test

---
+++ Test/Integration/ModuleTest.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace IMI\FriendlyCaptcha\Test\Integration;
+
+use IMI\FriendlyCaptcha\Api\ValidateInterface;
+use IMI\FriendlyCaptcha\Model\Validate;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Module\Manager as ModuleManager;
+use PHPUnit\Framework\TestCase;
+
+class ModuleTest extends TestCase
+{
+    public function testModuleIsEnabled(): void
+    {
+        $moduleManager = ObjectManager::getInstance()->get(ModuleManager::class);
+
+        self::assertTrue($moduleManager->isEnabled('IMI_FriendlyCaptcha'));
+    }
+
+    public function testValidateInterfacePreferenceIsConfigured(): void
+    {
+        $validate = ObjectManager::getInstance()->get(ValidateInterface::class);
+
+        self::assertInstanceOf(Validate::class, $validate);
+    }
+}
