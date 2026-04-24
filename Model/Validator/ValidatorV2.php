94e30c07b32aa770e367da8be6d5c27dfa48b03a
Alexander Menk <a.menk@imi.de>
2026-04-24 12:13:48 +0200

    V2 Support: Fix validation parameter name

--- Model/Validator/ValidatorV2.php
+++ Model/Validator/ValidatorV2.php
@@ -29,7 +29,7 @@ class ValidatorV2 extends AbstractValidator implements ValidateInterface
         ]);
 
         $curl->post($this->config->getVerifyEndpoint(), [
-            'solution' => $friendlyCaptchaSolution,
+            'response' => $friendlyCaptchaSolution,
             'sitekey' => $this->config->getSitekey(),
         ]);
 
